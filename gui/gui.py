7042bc95630988c9d997f6d411db1aca25eb1c2f
Agos95 <agostini.federico.95@gmail.com>
2019-02-07 11:29:18 +0100

    Basic GUI idea

---
+++ gui/gui.py
@@ -0,0 +1,84 @@
+import PySimpleGUI as sg
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('TkAgg')
+from matplotlib.backends.backend_tkagg import FigureCanvasAgg
+import matplotlib.backends.tkagg as tkagg
+import tkinter as Tk
+
+import analysis
+
+def draw_figure(canvas, figure, loc=(0, 0)):
+    figure_canvas_agg = FigureCanvasAgg(figure)
+    figure_canvas_agg.draw()
+    figure_x, figure_y, figure_w, figure_h = figure.bbox.bounds
+    figure_w, figure_h = int(figure_w), int(figure_h)
+    photo = Tk.PhotoImage(master=canvas, width=figure_w, height=figure_h)
+    canvas.create_image(loc[0] + figure_w/2, loc[1] + figure_h/2, image=photo)
+    tkagg.blit(photo, figure_canvas_agg.get_renderer()._renderer, colormode=2)
+    return photo
+
+############
+### MAIN ###
+############
+
+fig = plt.figure(figsize = (15, 6))
+background = analysis.plot_background()
+for ax in background: fig.add_subplot(ax)
+
+figure_x, figure_y, figure_w, figure_h = fig.bbox.bounds
+layout = [[sg.Text('Select File'), sg.InputText(key='_InputFileName_'), sg.FileBrowse(key='_InputFile_'), 
+           sg.Submit('Run the Analyses', key='_Analyze_'), sg.ProgressBar(10000, orientation='h', size=(30,20), key='_Progressbar_')], 
+          [sg.Listbox(values=[],size=(30, 30), bind_return_key=True, disabled=True, key='_EventList_'), sg.Canvas(size=(figure_w, figure_h), key='_Plot_')]]
+
+window = sg.Window('Event Display').Layout(layout)
+window.Finalize()
+progress_bar = window.FindElement('_Progressbar_')
+list_box = window.FindElement('_EventList_')
+plot = window.FindElement('_Plot_')
+
+fig_photo = draw_figure(plot.TKCanvas, fig)
+
+Events = {}
+
+while True:
+    event, values = window.Read(timeout=0)
+    # get filename when sent
+    data_file = ''
+    if event == '_Analyze_':
+        data_file = values['_InputFileName_']
+        with open(data_file) as f:
+            tot_lines = 0
+            for line in f: tot_lines += 1
+            progress_bar.UpdateBar(current_count=0, max=tot_lines)
+            f.seek(0) # to set the current position in the file to the first line 
+            #ev = {}
+            for line_number, line in enumerate(f):
+                event = line.split()
+                event = [float(i) for i in event]
+                dataframe, event_number, hits_number = analysis.read_data(event)
+                #sg.Print("Event", event_number, "- Hits:", hits_number)
+                Events.update({event_number:dataframe})
+                progress_bar.UpdateBar(line_number + 1)
+            
+        list_box.Update(values=sorted(Events), disabled=False)
+        
+        
+    elif event == '_EventList_':
+        #############
+        ### TO DO ###
+        #############
+        ### Need to clear the points before plotting anothr events
+        ### Need to handle events with no hits
+        
+        event_number = values['_EventList_'][0]
+        plt.clf()
+        axes = analysis.plot_events(Events[event_number], event_number)
+        for ax in axes: fig.add_subplot(ax)
+        fig_photo = draw_figure(plot.TKCanvas, fig)
+        window.Read()
+        
+    # exit
+    elif event is None: break
+
